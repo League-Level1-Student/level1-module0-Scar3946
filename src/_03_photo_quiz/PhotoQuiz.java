8bbe4d757e533459c641dfa7d917088c17630bf2
root <root@09f8409a03cf>
2021-01-12 00:57:58 +0000

    drumkit

--- src/_03_photo_quiz/PhotoQuiz.java
+++ src/_03_photo_quiz/PhotoQuiz.java
@@ -26,35 +26,47 @@ public class PhotoQuiz {
 		// 1. find an image on the internet, and put its URL in a String
 		// variable (from your browser, right click on the image, and select
 		// “Copy Image Address”)
-
+String endermen = "https://static.wikia.nocookie.net/hypixel-skyblock/images/2/28/Enderman.png/revision/latest?cb=20200209104412";
 		// 2. create a variable of type "Component" that will hold your image
-
+Component comp;
 		// 3. use the "createImage()" method below to initialize your Component
-
+comp = createImage(endermen);
 		// 4. add the image to the quiz window
-
+quizWindow.add(comp);
 		// 5. call the pack() method on the quiz window
-
+quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+String mining = JOptionPane.showInputDialog("What is this ?");
 		// 7. print "CORRECT" if the user gave the right answer
-
+if(mining.equalsIgnoreCase("enderman")) {
+	System.out.println("CORRECT");
+}
 		// 8. print "INCORRECT" if the answer is wrong
-
+else {
+	System.out.println("INCORRECT");
+}
 		// 9. remove the component from the quiz window (you may not see the
 		// effect of this until step 12)
-
+quizWindow.remove(comp);
 		// 10. find another image and create it (might take more than one line
 		// of code)
-
-		// 11. add the second image to the quiz window
-
+String ender = "https://upload.wikimedia.org/wikipedia/en/thumb/4/49/Creeper_%28Minecraft%29.png/220px-Creeper_%28Minecraft%29.png";
+	endermen = ender ;	
+// 11. add the second image to the quiz window
+quizWindow.add(comp)
 		// 12. pack the quiz window
-
+quizWindow.pack();
 		// 13. ask another question
-
+String creeper = JOptionPane.showInputDialog("what is this");
 		// 14+ check answer, say if correct or incorrect, etc.
-
+if (creeper.equalsIgnoreCase("a creeper")) {
+	System.out.println("CORRECT");
+}
+else{
+	System.out.println("INCORRECT");
+}
+	
+	
 	}
 
 	private Component createImage(String imageUrl) throws MalformedURLException {
