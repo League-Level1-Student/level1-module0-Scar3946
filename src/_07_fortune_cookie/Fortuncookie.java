cf515d35899e351fc5ac34d18a5afdfe604d297f
student <student@ip-172-31-49-28.us-west-2.compute.internal>
2021-02-10 16:54:05 -0800

    fortun cookie

--- src/_07_fortune_cookie/Fortuncookie.java
+++ src/_07_fortune_cookie/Fortuncookie.java
@@ -7,25 +7,35 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
-public class Fortuncookie {
-public void showButton() {
-	public class Fortuncookie implements ActionListener {
+
+
+public class Fortuncookie implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
-			
+JOptionPane.showMessageDialog(null, "woohoo");			
 		}
 		
-	}
+public void showButton () {
 	System.out.println("Button Clicked");
 	JFrame frame = new JFrame();
 	frame.setVisible(true);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	JButton button = new JButton();
+	button.setText("click me ");
 	frame.add(button);
 	frame.pack();
- JOptionPane.showMessageDialog(null, "woohoo");
- button.addActionListener(null);
-	
+ 
+ button.addActionListener(this);
+ int rand = new Random().nextInt(5);
+ if(rand = 1) {
+	 JOptionPane.showMessageDialog(null, "you will eat cheese");
+ }
+ //if (rand = 2) {
+	 //JOptionPane.showMessageDialog(null," you will eat an apple" );
+	 
+ }
+		}	
 }
-}
+
