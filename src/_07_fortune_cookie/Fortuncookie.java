e6ae358cd138fab63dc04259612a327a04ca0e52
root <root@f72f9ec74f67>
2021-02-16 00:59:56 +0000

    jackinthebox

--- src/_07_fortune_cookie/Fortuncookie.java
+++ src/_07_fortune_cookie/Fortuncookie.java
@@ -2,6 +2,7 @@ package _07_fortune_cookie;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -10,7 +11,6 @@ import javax.swing.JOptionPane;
 
 
 public class Fortuncookie implements ActionListener {
-
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
@@ -29,12 +29,16 @@ public void showButton () {
  
  button.addActionListener(this);
  int rand = new Random().nextInt(5);
- if(rand = 1) {
+ if(rand == 1) {
 	 JOptionPane.showMessageDialog(null, "you will eat cheese");
  }
- //if (rand = 2) {
-	 //JOptionPane.showMessageDialog(null," you will eat an apple" );
-	 
+ if (rand == 2) {
+	 JOptionPane.showMessageDialog(null," you will eat an apple" );
+if (rand == 3) {
+	JOptionPane.showMessageDialog(null, "you will play videogames");
+
+}
+
  }
 		}	
 }
---
+++ src/_08_jack_in_the_box/Jackintheboz.java
@@ -0,0 +1,8 @@
+package _08_jack_in_the_box;
+
+import javax.swing.JFrame;
+
+public class Jackintheboz {
+
+	JFrame frame = new JFrame();
+}
