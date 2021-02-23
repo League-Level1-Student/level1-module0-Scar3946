5483c4607a1e146381add720fd815cc1ef88672c
student <student@ip-172-31-61-3.us-west-2.compute.internal>
2021-02-22 17:00:48 -0800

    jack in the box

--- src/_08_jack_in_the_box/Jackintheboz.java
+++ src/_08_jack_in_the_box/Jackintheboz.java
@@ -1,8 +1,30 @@
 package _08_jack_in_the_box;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
-public class Jackintheboz {
-
-	JFrame frame = new JFrame();
+public class Jackintheboz implements ActionListener {
+  @Override
+  public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+JOptionPane.showMessageDialog(null, "woohoo");			
+	}
+  
+	public void showButton () {
+		System.out.println("Button Clicked");
+	
+		JFrame rame = new JFrame();
+		rame.setVisible(true);
+		rame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    JButton button = new JButton();
+		button.setText("Suprise");
+		rame.add(button);
+		rame.pack();
+		button.addActionListener(this);
+	 
+}
 }
